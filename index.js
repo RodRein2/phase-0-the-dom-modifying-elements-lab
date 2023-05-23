24918762285591a7145752e788ac6844b36ff9c3
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-05-23 15:00:18 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+let newHeader;
+
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+newHeader = document.createElement('h1');
+
+newHeader.id= 'victory';
+
+const yourName='Ana RR';
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
+
+
+
+
+
+// const newHeader = document.createElement(h1);
+// newHeader.id ='victory';
+
+// const yourName ='Ana RR';
+// newHeader.textContext =`${yourName}is the champion`;
+
+// document.body.appendChild(newHeader);
